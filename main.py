5d0dee071baa580e4ff46bef4450d540a1088bfb
Lucas Mayall <lucasmmayall@gmail.com>
2023-04-07 22:35:16 -0700

    Got random time generation setup, main loop is setup

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ class Event:
     def __init__(self, type: EventType, time: float, customer: Customer):
         self.time = time
         self.customer = customer
-        self.event_type = type
+        self.type = type
     
     def get_time(self):
         return self.time
@@ -99,17 +99,33 @@ class EventList:
     def dequeue(self):
         return self.splaytree.remove(self.splaytree.findMin())
 
+class CustomerStats:
+    def __init__(self):
+        self.arrival_time = 0.0
+        self.departure_time = 0.0
+        self.num_paintings_viewed = 0
+        self.total_viewing_time = 0.0
+        self.score_history = []
+
 class Customer:
-    def __init__(self, id: int, num_paintings: int):
-        self.id: int = id
+
+    CurrentID = 0
+
+    def __init__(self, num_paintings: int, rng):
+        self.id: int = Customer.CurrentID
+        Customer.CurrentID += 1
+
         self.favorite_style: Style = Style.random()
 
         # positive number, higher the tolerance the less affectec the customer is by the number of viewers
-        self.tolerance: float = np.random.normal(TOLERANCE_MEAN, TOLERANCE_STD)
-
-        self.arrival_time: float = np.random.normal(INTERARRIVAL_TIME_MEAN, INTERARRIVAL_TIME_STD)
+        self.tolerance: float = rng.normal(TOLERANCE_MEAN, TOLERANCE_STD)
 
         self.viewedPaintings = np.full(num_paintings, False)
+
+
+        self.stats = CustomerStats()
+       
+
 
     def beginViewing(self, painting: Painting, time: float):
         painting.num_viewers += 1
@@ -146,17 +162,36 @@ class Customer:
         return qualityScore + viewerScore + styleScore
 
 class SimStats:
-    def __init__(self, num_customers: int, num_paintings: int):
+    def __init__(self, num_customers, num_paintings):
         self.num_customers = num_customers
         self.num_paintings = num_paintings
 
         self.total_viewing_time = 0.0
+        self.num_arrived = 0
+        self.num_departed = 0
+        self.num_paintings_viewed = 0
+        self.num_leave_early = 0
+
+
+
+    def printStats(self):
+        average_viewing_time = self.total_viewing_time / self.num_paintings_viewed
+        average_paintings_viewed = self.num_paintings_viewed / self.num_customers
+
+        print("Average Viewing Time: " + str(average_viewing_time))
+        print("Average Paintings Viewed: " + str(average_paintings_viewed))
+        print("Number of Customers Arrived: " + str(self.num_arrived))
+        print("Number of Customers Departed: " + str(self.num_departed))
+        print("Number of Customers Left Early: " + str(self.num_leave_early))
+        
+
+
 
 
 
 
 class GallerySim:
-    def __init__(self, num_paintings: int, num_customers: int):
+    def __init__(self, num_paintings: int, num_customers: int, DEBUG=False):
         self.CustomersLeft = num_customers
 
         self.num_paintings = num_paintings
@@ -164,19 +199,123 @@ class GallerySim:
 
         self.paintings = [Painting(i, Style.random()) for i in range(num_paintings)]
 
+        self.stats = SimStats(self.num_customers, self.num_paintings)
+
         self.time = 0.0
         self.FutureEventList = EventList()
 
+        self.rng = np.random.default_rng()
+        self.customer = []
+
+        # Schedule the first arrival
+        self.ScheduleArrival()
+
+        while(self.stats.num_departed < self.num_customers):
+            # get next event
+            next_event = self.FutureEventList.dequeue()
+            self.time = next_event.time
+
+            if(DEBUG):
+                print("Event Type: " + str(next_event.type) + " Time: " + str(self.time))
+
+            # process event
+            if next_event.type == EventType.ARRIVAL:
+                print()
+                self.ProcessArrival(next_event)
+            elif next_event.type == EventType.DEPARTURE:
+                self.ProcessDeparture(next_event)
+            elif next_event.type == EventType.VIEWING:
+                self.ProcessViewing(next_event)
+            else:
+                raise Exception("Invalid Event Type")
+                
 
 
-    def processMove(self, customer: Customer):
+    def generateInterArrivalTime(self):
+        return self.rng.exponential(INTERARRIVAL_TIME_MEAN, INTERARRIVAL_TIME_STD)
+
+    def generateViewingTime(self):
+        return self.rng.normal(VIEWING_TIME_MEAN, VIEWING_TIME_STD)
+
+    def generateTolerance(self):
+        return self.rng.normal(TOLERANCE_MEAN, TOLERANCE_STD)
+
+    
+
+    def Start(self):
+        ## TODO, implement the while loop thing used in A3
+        pass
+
+
+    def ProcessArrival(self, evt: Event):
+        # So when the customer first arrives we basically just want to record the arrival then "move" them to their first painting
+        evt.customer.stats.arrived = True
+        self.stats.num_arrived += 1
+
+
+        # Schedule the next arrival
+        self.ScheduleArrival()
+
+
+
+
+        pass
+
+
+    def ScheduleArrival(self):
+        # Scheduling the next arrival        
+        
+        next_arrival_time = self.time + self.generateInterArrivalTime()
+
+        new_cust = Customer(self.num_paintings)
+        new_cust.arrival_time = next_arrival_time
+
+        # create the next arrival event
+        arrival_event = Event(EventType().ARRIVAL, next_arrival_time, new_cust)
+
+        self.customer.append(new_cust)
+
+        ## add the arrival to the future event list
+        self.FutureEventList.enqueue(arrival_event)
+
+    def ProcessDeparture(self, evt: Event):
+        self.stats.num_departed += 1
+
+
+
+    def ProcessMove(self, evt: Event):
+        # Min score at which point the customer will leave instead of going to the next painting
+        MIN_SCORE = 100
+
+        customer = evt.customer
+
         # get the painting with the highest score
-        best_painting = max(self.paintings, key=lambda p: customer.scorePainting(p))
+        painting_scores = np.array([customer.scorePainting(p) for p in self.paintings])
+
+        bestIndex = np.argmax(painting_scores)
+
+        if(painting_scores[bestIndex] < MIN_SCORE):
+            # If the person is leaving early
+            if(customer.stats.num_paintings_viewed < self.num_paintings):
+                self.stats.num_leave_early += 1
+
+            # EVent is now a departure
+            evt.type = EventType.DEPARTURE
+            self.ProcessDeparture(evt)
+
 
         # begin viewing the painting
         viewing_time = customer.beginViewing(best_painting, self.time)
+        customer.viewedPaintings[bestIndex] = True
 
-        # add the future event
+        self.stats.total_viewing_time += viewing_time
+
+        # Schedule the next MOVE
+        self.FutureEventList.enqueue(Event(EventType.MOVE, self.time + viewing_time))
+
+        customer.stats.total_viewing_time += viewing_time
+
+        return
 
 
 
