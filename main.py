281bcde6fa0c40c88dca8b0fa7acdb1adffacd1d
Sarah Clapoff <saraahclapoff@gmail.com>
2023-04-10 01:58:54 -0700

    finished flow of event creation/processing

--- main.py
+++ main.py
@@ -156,19 +156,18 @@ class EventList:
     
     def dequeue(self):
         #sarah: splaytree.remove() doesn't return the key it removes, so we have to do it ourselves
-        min_node = self.splaytree.findMin()
-        self.splaytree.remove(min_node)
-        return min_node
+        min_key = self.splaytree.findMin()
+        self.splaytree.remove(min_key)
+        return min_key
 
 class CustomerStats:
     def __init__(self):
         self.arrival_time = 0.0
-        self.departure_time = 0.0
+        self.departure_time = -1.0 #sarah: change to -1.0 to catch customers that have not yet left at end of simulation
         self.num_paintings_viewed = 0
         self.total_viewing_time = 0.0
         self.score_history = []
 
-#sarah: this is where customer class used to be
 
 class SimStats:
     def __init__(self, num_customers, num_paintings):
@@ -226,7 +225,7 @@ class GallerySim:
             self.time = next_event.time
 
             if(DEBUG):
-                print("Event Type: " + str(next_event.type) + " Time: " + str(self.time))
+                print("Event Type: " + str(next_event.type) + " Time: " + str(self.time) + " Customer: " + str(next_event.customer.id))
 
             # process event
             if next_event.type == EventType.ARRIVAL:
@@ -238,8 +237,17 @@ class GallerySim:
                 self.ProcessMove(next_event)
             else:
                 raise Exception("Invalid Event Type")
-            #increment num_departed so the program ends (just here while testing)
-            #self.stats.num_departed += 1
+
+        #end of sim, print report:
+        #we only want to consider the first customer_number departures, so delete the customers that have not yet departed
+        self.customer = [i for i in self.customer if i.stats.departure_time != -1.0 ]
+        
+        #print([c.id for c in self.customer])
+        #self.stats.printStats() #problem: float division by zero for customers who leave w/out seeing any paintings
+        # print('\n customer stats:')
+        # print(['arrival time %.4f, depart time: %.4f, num paintings: %.4f, total view time: %.4f '
+        #       %(c.stats.arrival_time, c.stats.departure_time, c.stats.num_paintings_viewed, c.stats.total_viewing_time) for c in self.customer])
+        # print('customer score histories: ', [ str(c.stats.score_history) for c in self.customer])
                 
 
 
@@ -260,18 +268,15 @@ class GallerySim:
         # So when the customer first arrives we basically just want to record the arrival then "move" them to their first painting
         evt.customer.stats.arrived = True
         self.stats.num_arrived += 1
+        evt.customer.stats.arrival_time = evt.time #update stats for this customer 
 
-        #process initial move (this occurs immidiately after customer arrives, based on current system state)
+        #process initial move (this occurs immidiately after customer arrives)
         evt.type = EventType.MOVE #event is now Move
         self.ProcessMove(evt)
 
         # Schedule the next arrival
         self.ScheduleArrival()
 
-
-
-
-        pass
 
 
     def ScheduleArrival(self):
@@ -290,8 +295,14 @@ class GallerySim:
         ## add the arrival to the future event list
         self.FutureEventList.enqueue(arrival_event)
 
+
     def ProcessDeparture(self, evt: Event):
+        #departures will always be processed directly from a Move (ie. not from main loop),
+        # so the event will have already been removed from futureEventsList
+        #all we need to do is update stats:
         self.stats.num_departed += 1
+        evt.customer.stats.departure_time = evt.time
+        print('customer', evt.customer.id, 'leaving')
 
 
 
@@ -300,12 +311,11 @@ class GallerySim:
         MIN_SCORE = 100
 
         customer = evt.customer
-
         # get the painting with the highest score
         painting_scores = np.array([customer.scorePainting(p) for p in self.paintings])
-        best_painting: Painting #placeholder to define variable (fix!!)
 
         bestIndex = np.argmax(painting_scores)
+        best_painting = self.paintings[bestIndex]
 
         if(painting_scores[bestIndex] < MIN_SCORE):
             # If the person is leaving early
@@ -315,6 +325,7 @@ class GallerySim:
             # EVent is now a departure
             evt.type = EventType.DEPARTURE
             self.ProcessDeparture(evt)
+            return #otherwise customer continues to view painting
 
 
         # begin viewing the painting
@@ -323,10 +334,12 @@ class GallerySim:
 
         self.stats.total_viewing_time += viewing_time
 
-        # Schedule the next MOVE
-        self.FutureEventList.enqueue(Event(EventType.MOVE, self.time + viewing_time))
+        # Schedule the next MOVE (sarah: for this customer right?)
+        self.FutureEventList.enqueue(Event(EventType.MOVE, self.time + viewing_time, customer))
 
         customer.stats.total_viewing_time += viewing_time
+        customer.stats.num_paintings_viewed += 1
+        customer.stats.score_history.append((best_painting.id, painting_scores[bestIndex])) #adding as tuple so we can keep track of both
 
         return
 
